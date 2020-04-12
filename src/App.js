85e6f10801f1d7d05730ef73b9e00a178403492a
cayleatopolski <woahitscaylea@gmail.com>
2020-04-12 15:23:55 -0400

    Refactored to get data from an array to mimic data from an api or db.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
 import "./App.css";
 import Joke from "./Joke";
+import jokesData from "./jokesData";
 
 const App = () => {
-  return (
-    <div>
-      <Joke
-        question="Why do we tell actors to 'break a leg?'"
-        answer="Because every play has a cast."
-      />
-      <Joke
-        question="What's the best thing about Switzerland?"
-        answer="I don't know, but the flag is a big plus."
-      />
-      <Joke
-        question="Did you hear about the mathematician who's afraid of negative numbers?"
-        answer="He'll stop at nothing to avoid them."
-      />
-      <Joke
-        question="Hear about the new restaurant called Karma?"
-        answer="There’s no menu: You get what you deserve."
-      />
-      <Joke answer="I threw a boomerang a few years ago. I now live in constant fear." />
-    </div>
-  );
+  const jokeComponents = jokesData.map((joke) => (
+    <Joke key={joke.id} question={joke.question} answer={joke.answer} />
+  ));
+
+  return <div>{jokeComponents}</div>;
 };
 
 export default App;
---
+++ src/jokesData.js
@@ -0,0 +1,34 @@
+const jokesData = [
+  {
+    id: 1,
+    answer: "I threw a boomerang a few years ago. I now live in constant fear.",
+  },
+  {
+    id: 2,
+    question: "What's the best thing about Switzerland?",
+    answer: "I don't know, but the flag is a big plus!",
+  },
+  {
+    id: 3,
+    question:
+      "Did you hear about the mathematician who's afraid of negative numbers?",
+    answer: "He'll stop at nothing to avoid them!",
+  },
+  {
+    id: 4,
+    question: "Hear about the new restaurant called Karma?",
+    answer: "There’s no menu: You get what you deserve.",
+  },
+  {
+    id: 5,
+    question: "Did you hear about the actor who fell through the floorboards?",
+    answer: "He was just going through a stage.",
+  },
+  {
+    id: 6,
+    question: "Did you hear about the claustrophobic astronaut?",
+    answer: "He just needed a little space.",
+  },
+];
+
+export default jokesData;
